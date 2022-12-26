edb0bf2e1e732ef0d867dbb95592925634281342
Vladislav Kuznetsov <gloomy.fox123@gmail.com>
2022-12-26 19:20:35 +0300

    13#5 Deleted all comments

--- erorrs/incorrect-date.js
+++ erorrs/incorrect-date.js
@@ -1,7 +1,6 @@
 class IncorrectDateError extends Error {
   constructor(message) {
     super(message);
-    // this.name = 'ValidationError';
     this.name = 'IncorrectDateError';
     this.statusCode = 400;
   }
