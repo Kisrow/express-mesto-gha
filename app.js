0c937fa7bf9401ac73c89b7c7ee966c9b18227b3
Vladislav Kuznetsov <gloomy.fox123@gmail.com>
2023-01-08 13:40:19 +0300

    for test 14

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { login, createUser } = require('./controllers/users');
-// const auth = require('./middlewares/auth');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -12,19 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-// временное решение авторизации
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '63a6e40600178ca6b6b9d91c',
-//   };
-
-//   next();
-// });
-
 app.post('/signup', createUser);
 app.post('/signin', login);
 
-// app.use(auth);
+app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 app.use('*', require('./routes/not-found-request'));
